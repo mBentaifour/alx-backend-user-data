b8aefe0e31e9c790ee5f4c270904419a68f93dac
mbentaifour <bentaifourmoh@gmail.com>
2024-11-21 15:03:50 +0000

    User authentication service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     SQLAlchemy model for the 'users' table.
@@ -19,4 +20,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
